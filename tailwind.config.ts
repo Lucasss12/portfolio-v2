ef67b57dbf23d6b09fec51198e386c3aec00ebdb
Lucasss12 <lucas.mene.pro@gmail.com>
2024-08-06 16:58:27 +0200

    add dock

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,52 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  // darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        'pulse-shadow': {
+          '0%, 100%': {
+            boxShadow: '0 0 10px 4px rgba(59, 229, 31, 0.6)',
+          },
+          '50%': {
+            boxShadow: '0 0 10px 4px rgba(59, 229, 31, 1)',
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        'pulse-shadow': 'pulse-shadow 1.5s infinite',
+      },
+      boxShadow: {
+        'custom': 'rgba(99, 99, 99, 0.15) 0px 2px 4px 0px',
+      },
+      colors: {
+        textGray: '#999999',
+        linkActive: 'rgba(151, 197, 248, 0.1)',
+        gradiantBlue: 'rgba(16, 119, 215, 1)',
+        gradiantAccentBlue: 'rgba(151, 197, 248, 1)',
+        greenAccent: '#3BE51F',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
