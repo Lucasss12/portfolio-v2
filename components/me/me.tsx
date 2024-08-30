1de364bea098701554a4e971591b94c0774bf02c
Lucasss12 <lucas.mene.pro@gmail.com>
2024-08-30 23:36:00 +0200

    fix deploy

--- components/me/me.tsx
+++ components/me/me.tsx
@@ -16,7 +16,7 @@ export default function me() {
       <p>
         Après avoir validé mon bootcamp, j&apos;ai continué à me former en
         autodidacte sur des principes et technologies plus avancés comme {""}
-        <span className="underline text-black">Next.js</span>, <span className="underline text-black">l'UI/UX </span> ou encore <span className="underline text-black">Figma</span>. C'est à ce moment-là que j'ai
+        <span className="underline text-black">Next.js</span>, <span className="underline text-black">l&apos;UI/UX </span> ou encore <span className="underline text-black">Figma</span>. C&apos;est à ce moment-là que j&apos;ai
         choisi de me spécialiser dans le développement <span className="underline text-black">front-end</span> et de me lancer
         dans le freelancing.
       </p>
