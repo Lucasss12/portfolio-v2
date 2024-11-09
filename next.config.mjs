7c4d249e9ead5e47c2b2bc70e52d2f6b90b4ed05
Lucasss12 <lucas.mene.pro@gmail.com>
2024-11-09 23:26:48 +0100

    test fix images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     metadataBase: 'https://lucas-mene.vercel.app/',
   },
+  images: {
+    domains: ['https://lucas-mene.vercel.app/'], 
+  },
 };
 
 export default nextConfig;
